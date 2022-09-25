7f7c4b010e6a2333e970482b9ddb33b7584f28c5
Ashutosh Mishra <iashutoshmishra8@gmail.com>
2022-09-25 19:16:31 +0530

    add b, u, & copy buttons on note's card

--- script.js
+++ script.js
@@ -10,6 +10,23 @@ const inputNote = document.querySelector(".input-note");
 const articleElement = document.createElement("article");
 const pTitleElement = document.createElement("p");
 const pNoteElement = document.createElement("p");
+const divLeft = document.createElement("div");
+const divRight = document.createElement("div");
+const div = document.createDocumentFragment("div")
+const bold = document.createElement("button");
+const underline = document.createElement("button");
+const copy = document.createElement("button");
+const footer = document.createElement("footer");
+
+
+bold.innerHTML = "B";
+underline.innerHTML = "U";
+copy.innerHTML = "C";
+
+console.log(bold);
+console.log(underline);
+console.log(copy);
+console.log(articleElement);
 
 // Click event listeners
 newNoteButton.addEventListener("click", displayNoteForm);
@@ -23,6 +40,11 @@ inputNote.addEventListener("change", newNoteContent);
   articleElement.classList.add("note-box");
   pTitleElement.classList.add("note-headings");
   pNoteElement.classList.add("notes");
+  divLeft.classList.add("left");
+  divRight.classList.add("right");
+  bold.classList.add("bold");
+  underline.classList.add("underline");
+  copy.classList.add("copy");
 })();
 
 // Add the title to the new note
@@ -50,9 +72,27 @@ function displayNoteForm() {
 function createNote(e) {
   e.preventDefault();
   console.log(noteSection);
-  articleElement.appendChild(pTitleElement);
-  articleElement.appendChild(pNoteElement);
+
+  divLeft.appendChild(bold);
+  divLeft.appendChild(underline);
+  divRight.appendChild(copy);
+
+
+  footer.appendChild(divLeft);
+  footer.appendChild(divRight);
+
+  div.appendChild(pTitleElement);
+  div.appendChild(pNoteElement);
+  articleElement.appendChild(div)
+  articleElement.appendChild(footer)
+
+  console.log(pNoteElement);
+
+  console.log(bold);
   noteSection.appendChild(articleElement.cloneNode(true));
+  
+  console.log(footer)
+
   document.querySelector(".input-form").style.display = "none";
   inputTitle.value = "";
   inputNote.value = "";
